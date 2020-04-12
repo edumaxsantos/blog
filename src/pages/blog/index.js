634212476ab958c3eb987958cee7119c140324a2
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2020-04-12 02:24:35 -0300

    Changed component classes to functions

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,18 +3,16 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
-export default class BlogIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
-          </div>
-        </section>
-      </Layout>
-    )
-  }
-}
+const BlogIndexPage = () => (
+  <Layout>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <BlogRoll />
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default BlogIndexPage
